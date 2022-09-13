fe45237671b58c7cb84c83439cde3c852f286480
gratechs <godimfon@gmail.com>
2022-09-13 07:18:50 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        benchnine: ['BenchNine', 'sans-serif'],
+        nunitosans: ['Nunito Sans', 'sans-serif'],
+        playfair: ['Playfair Display', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
