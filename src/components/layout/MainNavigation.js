749f47cbee7ac3f6bb50be33d5517fe55fd0524c
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2022-10-21 14:46:50 +0100

    feat: Add menu navigation functionality in mobile and tablet view

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
+
 import logoImage from '../../assets/logo.svg';
 
 const MainNavigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const overflowY = 'overflow-y-hidden';
+
+  const navToggleHandler = () => {
+    setIsOpen(prevState => !prevState);
+    document.body.classList.toggle(overflowY);
+  };
+
   return (
     <div className="box-container pt-7 pb-4 md:pt-9 lg:pt-8">
       <nav className="flex justify-between items-center font-extrabold">
@@ -33,7 +44,49 @@ const MainNavigation = () => {
         >
           Request today
         </a>
+
+        {/* Hamburger Button */}
+        <div className="md:hidden">
+          <button
+            className={`z-20 fixed hamburger block focus:outline-none md:hidden ${
+              isOpen ? 'open' : ''
+            }`}
+            onClick={navToggleHandler}
+          >
+            <div className="hamburger-top"></div>
+            <div className="hamburger-middle"></div>
+            <div className="hamburger-bottom"></div>
+          </button>
+        </div>
       </nav>
+
+      {/* Mobile Menu */}
+      <div
+        className={`z-10 fixed top-0 right-0 flex flex-col space-y-8 h-screen w-0 pl-10 py-20 text-2xl text-zinc-500 font-playfair font-bold bg-blackGray  opacity-0 duration-500 transition-all ease-[cubic-bezier(0.5,1,0.89,1] md:hidden ${
+          isOpen ? 'w-[98%] opacity-100' : ''
+        }`}
+      >
+        <a href="#themes" className="hover:text-white">
+          Themes
+        </a>
+        <a href="#features" className="hover:text-white">
+          Features
+        </a>
+        <a href="#price" className="hover:text-white">
+          Price list
+        </a>
+        <a href="#faq" className="hover:text-white">
+          Faq
+        </a>
+        <div>
+          <a
+            href="mailto:godimfon@gmail.com"
+            className="inline-block px-6 py-3 text-base text-saffron border border-saffronDark rounded hover:border-saffron"
+          >
+            Request today
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
